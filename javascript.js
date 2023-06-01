a799741d9c0f89019462faafee0c52844e1fde77
ChrisT200456215 <132523544+ChrisT200456215@users.noreply.github.com>
2023-06-01 19:31:33 -0400

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,10 @@
+// script.js
+
+// Function to display "Hello, world!" message
+function sayHello() {
+    console.log("Hello, world!");
+  }
+  
+  // Call the sayHello function
+  sayHello();
+  
